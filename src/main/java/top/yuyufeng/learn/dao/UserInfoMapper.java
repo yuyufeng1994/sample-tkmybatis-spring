4b3b2408a6910c2c6476bc357223b7937eae7252
yuyufeng <416530244@qq.com>
2018-02-27 17:05:58 +0800

    增加generator

--- src/main/java/top/yuyufeng/learn/dao/UserInfoMapper.java
+++ src/main/java/top/yuyufeng/learn/dao/UserInfoMapper.java
@@ -3,9 +3,5 @@ package top.yuyufeng.learn.dao;
 import tk.mybatis.mapper.common.Mapper;
 import top.yuyufeng.learn.modal.UserInfo;
 
-/**
- * @author yuyufeng
- */
 public interface UserInfoMapper extends Mapper<UserInfo> {
-
 }
--- src/main/java/top/yuyufeng/learn/modal/UserInfo.java
+++ src/main/java/top/yuyufeng/learn/modal/UserInfo.java
@@ -1,52 +1,88 @@
 package top.yuyufeng.learn.modal;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Date;
+import javax.persistence.*;
 
 @Table(name = "user_info")
 public class UserInfo {
     @Id
-    @GeneratedValue(generator = "JDBC")
+    @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "user_name")
     private String userName;
 
+    @Column(name = "create_time")
     private Date createTime;
 
+    @Column(name = "user_email")
     private String userEmail;
 
+    public UserInfo(Long userId, String userName, Date createTime, String userEmail) {
+        this.userId = userId;
+        this.userName = userName;
+        this.createTime = createTime;
+        this.userEmail = userEmail;
+    }
+
+    public UserInfo() {
+        super();
+    }
+
+    /**
+     * @return user_id
+     */
     public Long getUserId() {
         return userId;
     }
 
+    /**
+     * @param userId
+     */
     public void setUserId(Long userId) {
         this.userId = userId;
     }
 
+    /**
+     * @return user_name
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * @param userName
+     */
     public void setUserName(String userName) {
-        this.userName = userName;
+        this.userName = userName == null ? null : userName.trim();
     }
 
+    /**
+     * @return create_time
+     */
     public Date getCreateTime() {
         return createTime;
     }
 
+    /**
+     * @param createTime
+     */
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
 
+    /**
+     * @return user_email
+     */
     public String getUserEmail() {
         return userEmail;
     }
 
+    /**
+     * @param userEmail
+     */
     public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
+        this.userEmail = userEmail == null ? null : userEmail.trim();
     }
 
     @Override
